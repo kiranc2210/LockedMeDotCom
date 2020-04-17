8f035eeeb1f110037dbe175ffdeab76ab1247921
kiranc2210 <kiranc2210@gmail.com>
2020-04-17 11:39:45 +0530

    LockedMeDotCom Project Assignment: Version 1.1-- modified Addfile and getFileByName methods Handled some Exceptions

---
+++ LockedMeDotCom    V1.1/LockedMeDotCom/src/main/java/com/LockedMeDotCom/bussinessObject/implimentations/LockedMeBoImpl.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package com.LockedMeDotCom.bussinessObject.implimentations;
+
+import java.io.File;
+
+import com.LockedMeDotCom.bussinessObject.LockedMeBOInterface;
+import com.LockedMeDotCom.dataAccessObject.LockedMeDaoInterface;
+import com.LockedMeDotCom.dataAccessObject.implimentaions.LockedMeDaoImpl;
+
+/**
+ * @author kiran
+ *
+ */
+public class LockedMeBoImpl implements LockedMeBOInterface {
+
+	private LockedMeDaoInterface lmd;
+
+	private LockedMeDaoInterface getLMD() {
+		if (lmd == null) {
+			lmd = new LockedMeDaoImpl();
+		}
+		return lmd;
+	}
+
+	public File addUserFile(String fileName) throws Exception {
+
+		String userFileName = "";
+		String[] filename = fileName.split(" ");
+		for (String name : filename) {
+			userFileName = name.substring(0, 1).toUpperCase() + name.substring(1);
+		}
+
+		File fn = getLMD().addUserFile(userFileName);
+
+		return fn;
+	}
+
+	public String getFileByName(String fileName) throws Exception {
+
+		String file = "";
+		String fname = getLMD().getFileByName(fileName);
+
+		if (fname.equals(fileName)) {
+			file = fname;
+			System.out.println("File Found is: " + file);
+		} else {
+			System.out.println(fileName + " " + "does not exist");
+		}
+
+		return file;
+	}
+
+	public void updateFile(String fileName, String fileContents) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String readContentsFromFile(String fileName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteUserFile(String fileName) throws Exception {
+
+		getLMD().deleteUserFile(fileName).delete();
+
+		System.out.println("File has been Deleted");
+
+	}
+
+	public void getAllFileNames() throws Exception {
+
+		// java.util.List<File>
+		// fileName=(getLMD().getAllFileNames()).stream().sorted().collect(Collectors.toList());
+		// fileName.stream().forEach(System.out::println);
+		java.util.List<String> fileName = getLMD().getAllFileNames();
+
+		int listSize = fileName.size();
+		if (listSize == 0) {
+			System.out.println("Directory is Empty");
+
+		} else {
+			for (String file : fileName) {
+				String name = file.toString();
+
+				System.out.print(name + "\n");
+
+			}
+
+		}
+		System.out.println("Totoal number of file Available in the Directory:" + listSize);
+
+	}
+}
---
+++ LockedMeDotCom    V1.1/LockedMeDotCom/src/main/java/com/LockedMeDotCom/presentationView/Main.java
@@ -0,0 +1,177 @@
+/**
+ * 
+ */
+package com.LockedMeDotCom.presentationView;
+
+import java.util.Scanner;
+
+import com.LockedMeDotCom.bussinessObject.LockedMeBOInterface;
+import com.LockedMeDotCom.bussinessObject.implimentations.LockedMeBoImpl;
+
+/**
+ * @author kiran
+ *
+ */
+public class Main {
+	LockedMeBOInterface lockedMeBOI;
+
+	/**
+	 * @param args
+	 */
+	public void subMian() {
+		lockedMeBOI = new LockedMeBoImpl();
+		System.out.println("\n\n");
+
+		System.out.println("****File Available in the Current Directory****\n");
+		try {
+			lockedMeBOI.getAllFileNames();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
+		Scanner scan = new Scanner(System.in);
+
+		int choice = 0;
+
+		System.out.println("Main Manu operations");
+		System.out.println("*********************\n");
+		do {
+			System.out.println("1. Add a file to the existing directory with File Extension\r\n");
+			System.out.println("2. Get File by providing the FileName\r\n");
+			System.out.println("3. Update the File by Entering fileName and Contents of file\r\n");
+			System.out.println("4. Delete the file by fileName whit Extension \r\n");
+			System.out.println("5. exite context\r\n");
+			System.out.println("Enter your choice");
+
+			try {
+
+				choice = Integer.parseInt(scan.nextLine());
+
+			} catch (NumberFormatException e) {
+
+			}
+
+			switch (choice) {
+
+			case 1:
+				System.out.println(
+						"***Enter the File Name to create new file(FileName Should Contain Atleast 3 character):***");
+
+				try {
+					String fileName = scan.nextLine();
+
+					lockedMeBOI.addUserFile(fileName);
+					System.out.println("*********************");
+					System.out.println("\n");
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+
+			case 2:
+				System.out.println("***Get file by providing specified File Name:***");
+				try {
+
+					String fileName = scan.nextLine();
+					lockedMeBOI.getFileByName(fileName);
+					System.out.println("*********************");
+					System.out.println("\n");
+					Runtime.getRuntime()
+							.exec("notepad" + " " + System.getProperty("user.dir") + "/LockedMeDotCom/" + fileName);
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+
+				}
+				break;
+			case 3:
+				System.out.println("****Update the Existing File with new contents*****");
+				System.out.println("**Enter the FileName to update**");
+				String fileName = scan.nextLine();
+
+				System.out.println("**Enter contents to be updated**");
+				String contents = scan.nextLine();
+				try {
+					lockedMeBOI.updateFile(fileName, contents);
+				} catch (Exception e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+
+				break;
+			case 4:
+				System.out.println("***Enter the file name to delete the file from directory:***");
+				String filename = scan.nextLine();
+				try {
+					lockedMeBOI.getFileByName(filename);
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.out.println("**Are you sure want to delete? Yes Or No**");
+				if ((scan.nextLine()).equalsIgnoreCase("Yes")) {
+					try {
+						lockedMeBOI.deleteUserFile(filename);
+						System.out.println("*********************");
+						System.out.println("\n");
+						new Main().subMian();
+
+					} catch (Exception e) {
+
+						e.printStackTrace();
+					}
+				} else {
+					System.out.println("*********************");
+					System.out.println("\n");
+					new Main().subMian();
+				}
+				break;
+			case 5:
+				System.out.println("Exit from the Application");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("****File Available in the Current Directory****");
+				try {
+					lockedMeBOI.getAllFileNames();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("Entered choice is invalid. Choice should be between 1 to 6");
+				break;
+			}
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} while (choice != 5);
+		scan.close();
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("\t\t\t\tWelcome to LockedMe.com V1.0");
+		System.out.println();
+
+		System.out.print("ProjectName" + " " + ":" + "   " + "LOCKEDME.COM\t\t\t");
+		System.out.println("\t\tAuthorName" + "  " + ":" + "   " + "Kiran Kumar C");
+		System.out.println();
+
+		Main main = new Main();
+		main.subMian();
+
+	}
+
+}
